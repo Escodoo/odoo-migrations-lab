9a0d41287aad42d9c75ac35d6470c9b4c033c402
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-05-11 13:02:06 -0300

    First commit (v12 em ambos os ambientes)

---
+++ op_dest/oca/l10n-brazil/l10n_br_account/models/account_tax.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2009 - TODAY Renato Lima - Akretion
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class AccountTax(models.Model):
+    _inherit = 'account.tax'
+
+    fiscal_tax_ids = fields.Many2many(
+        comodel_name='l10n_br_fiscal.tax',
+        relation='l10n_br_fiscal_account_tax_rel',
+        colunm1='account_tax_id',
+        colunm2='fiscal_tax_id',
+        string='Fiscal Taxes')
+
+    @api.multi
+    def compute_all(
+        self,
+        price_unit,
+        currency=None,
+        quantity=None,
+        product=None,
+        partner=None,
+        fiscal_taxes=None,
+        operation_line=False,
+        ncm=None,
+        nbm=None,
+        cest=None,
+        discount_value=None,
+        insurance_value=None,
+        other_costs_value=None,
+        freight_value=None,
+        fiscal_price=None,
+        fiscal_quantity=None,
+        uot=None,
+        icmssn_range=None
+    ):
+        """ Returns all information required to apply taxes
+            (in self + their children in case of a tax goup).
+            We consider the sequence of the parent for group of taxes.
+                Eg. considering letters as taxes and alphabetic order
+                as sequence :
+                [G, B([A, D, F]), E, C] will be computed as [A, D, F, C, E, G]
+        RETURN: {
+            'total_excluded': 0.0,    # Total without taxes
+            'total_included': 0.0,    # Total with taxes
+            'taxes': [{               # One dict for each tax in self
+                                      # and their children
+                'id': int,
+                'name': str,
+                'amount': float,
+                'sequence': int,
+                'account_id': int,
+                'refund_account_id': int,
+                'analytic': boolean,
+            }]
+        } """
+
+        taxes_results = super(AccountTax, self).compute_all(
+            price_unit, currency, quantity, product, partner)
+
+        if not fiscal_taxes:
+            fiscal_taxes = self.env['l10n_br_fiscal.tax']
+
+        # FIXME Should get company from document?
+        fiscal_taxes_results = fiscal_taxes.compute_taxes(
+            company=self.env.user.company_id,
+            partner=partner,
+            product=product,
+            price_unit=price_unit,
+            quantity=quantity,
+            uom_id=product.uom_id,
+            fiscal_price=fiscal_price,
+            fiscal_quantity=fiscal_quantity,
+            uot_id=uot or product.uot_id,
+            ncm=ncm or product.ncm_id,
+            nbm=nbm or product.nbm_id,
+            cest=cest or product.cest_id,
+            discount_value=discount_value,
+            insurance_value=insurance_value,
+            other_costs_value=other_costs_value,
+            freight_value=freight_value,
+            operation_line=operation_line,
+            icmssn_range=icmssn_range)
+
+        account_taxes_by_domain = {}
+        for tax in self:
+            tax_domain = tax.tax_group_id.fiscal_tax_group_id.tax_domain
+            account_taxes_by_domain.update({tax.id: tax_domain})
+
+        for account_tax in taxes_results['taxes']:
+            fiscal_tax = fiscal_taxes_results.get(
+                account_taxes_by_domain.get(
+                    account_tax.get('id')))
+
+            if fiscal_tax:
+                if not fiscal_tax.get('tax_include'):
+                    taxes_results['total_included'] += fiscal_tax.get(
+                        'tax_value')
+
+                account_tax.update({
+                    'id': account_tax.get('id'),
+                    'name': fiscal_tax.get('name'),
+                    'amount': fiscal_tax.get('tax_value'),
+                    'base': fiscal_tax.get('base'),
+                    'tax_include': fiscal_tax.get('tax_include')
+                })
+
+        return taxes_results
---
+++ op_dest/oca/l10n-brazil/l10n_br_portal/tests/test_load_process.py
@@ -0,0 +1,20 @@
+# Copyright 2016 KMEE - Luis Felipe Miléo <mileo@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
+
+import odoo.tests
+
+
+@odoo.tests.tagged('post_install', '-at_install')
+class TestUi(odoo.tests.HttpCase):
+    def test_01_l10n_br_portal_load_tour(self):
+        self.phantom_js(
+            "/",
+            "odoo.__DEBUG__.services['web_tour.tour'].run('l10n_br_portal_tour')",  # noqa: E501
+            "odoo.__DEBUG__.services['web_tour.tour'].tours.l10n_br_portal_tour.ready",  # noqa: E501
+            login="portal"
+        )
+        # check result
+        record = self.env.ref('base.partner_demo_portal')
+        self.assertEqual(record.country_id.code, 'BR')
+        self.assertEqual(record.state_id.code, 'MG')
+        self.assertEqual(record.city_id.ibge_code, '32404')
